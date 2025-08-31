9090cc0a354e7855fcce07a38f80eefdbc9aa987
lisadonlon <lisa@donlonlsc.com>
2025-08-31 19:03:28 +0100

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Wait for the credentials.json file to be available.
+while [ ! -f credentials.json ]; do
+  echo "Waiting for credentials.json to be mounted..."
+  sleep 1
+done
+
+echo "credentials.json found. Starting Gunicorn..."
+# Start the Gunicorn server.
+gunicorn app:app
